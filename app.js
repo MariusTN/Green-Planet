8f30ac0150a0f212d63c92627413ab32737bf677
MariusTN <pulsemusic8@gmail.com>
2020-10-22 18:58:07 +0100

    needs changes at around 600px / hero img / buttons in nav / everything is design only, no function what-so-ever

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// OOP - Hallo querySelector
-let nav = document.querySelector('.nav-links');
-let burger = document.querySelector('.burger');
-let links = nav.querySelectorAll('a');
+// // OOP - Hallo querySelector
+// let nav = document.querySelector('.nav-links');
+// let burger = document.querySelector('.burger');
+// let links = nav.querySelectorAll('a');
 
-burger.addEventListener("click", () =>{
-    nav.classList.toggle("nav-open");
-    burger.classList.toggle("toggle");
-});
+// burger.addEventListener("click", () =>{
+//     nav.classList.toggle("nav-open");
+//     burger.classList.toggle("toggle");
+// });
 
-links.forEach(link => {
-    link.addEventListener('click', ()=> {
-        nav.classList.toggle('nav-open');
-        burger.classList.toggle("toggle");
-    });
-});
+// links.forEach(link => {
+//     link.addEventListener('click', ()=> {
+//         nav.classList.toggle('nav-open');
+//         burger.classList.toggle("toggle");
+//     });
+// });
